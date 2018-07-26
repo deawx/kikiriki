fdc8d05e9df811d7c05c4ece9e55fce8723403de
Eleftherios Psitopoulos <lefpsi@gmail.com>
2018-07-27 02:49:16 +0300

    clock styling

--- src/AlarmClock.js
+++ src/AlarmClock.js
@@ -4,37 +4,81 @@ import "./AlarmClock.css";
 class AlarmClock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            hours: 0,
+            minutes: 0
+        };
     }
+
     render() {
         return (
             <div className="alarm-clock">
                 <div className="alarm-clock__screen">
-                    <span className="alarm-clock__screen-hours">11</span>
-                    <span className="alarm-clock__screen-minutes">11</span>
-                    <span className="alarm-clock__screen-seconds">11</span>
+                    <span className="alarm-clock__screen-hours">
+                        {this.twoDigits(this.state.hours)}
+                    </span>
+                    <span className="alarm-clock__screen-minutes">
+                        {this.twoDigits(this.state.minutes)}
+                    </span>
                 </div>
                 <div className="alarm-clock__controls">
                     <div className="alarm-clock__controls--hour">
-                        <span className="alarm-clock__controls--hour-up">
-                            up
+                        <span
+                            onClick={() => this.increaseHours()}
+                            className="alarm-clock__controls--hour-up"
+                        >
+                            +
                         </span>
-                        <span className="alarm-clock__controls--hour-down">
-                            down
+                        <span
+                            onClick={() => this.decreaseHours()}
+                            className="alarm-clock__controls--hour-down"
+                        >
+                            -
                         </span>
                     </div>
                     <div className="alarm-clock__controls--minutes">
-                        <span className="alarm-clock__controls--minutes-up">
-                            up
+                        <span
+                            onClick={() => this.increaseMinutes()}
+                            className="alarm-clock__controls--minutes-up"
+                        >
+                            +
                         </span>
-                        <span className="alarm-clock__controls--minutes-down">
-                            down
+                        <span
+                            onClick={() => this.decreaseMinutes()}
+                            className="alarm-clock__controls--minutes-down"
+                        >
+                            -
                         </span>
                     </div>
                 </div>
             </div>
         );
     }
+
+    increaseHours = () => {
+        let { hours } = this.state;
+        hours = hours < 23 ? Number(hours + 1) : 0;
+        this.setState({ hours });
+    };
+    decreaseHours = () => {
+        let { hours } = this.state;
+        hours = hours > 1 ? Number(hours - 1) : 23;
+        this.setState({ hours });
+    };
+    increaseMinutes = () => {
+        let { minutes } = this.state;
+        minutes = minutes < 59 ? Number(minutes + 1) : 0;
+        this.setState({ minutes });
+    };
+    decreaseMinutes = () => {
+        let { minutes } = this.state;
+        minutes = minutes > 0 ? Number(minutes - 1) : 59;
+        this.setState({ minutes });
+    };
+
+    twoDigits = number => {
+        return number < 10 ? "0" + number : number;
+    };
 }
 
 export default AlarmClock;
